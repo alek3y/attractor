a202307faea406b77071be1e6b22cb084a3ffa7d
alek3y <alek3y@protonmail.com>
2022-08-14 16:10:33 +0200

    Add multiple randomly generated points

--- js/attractor.js
+++ js/attractor.js
@@ -1,44 +1,74 @@
 class Attractor {
-	constructor(scene, {sigma, beta, rho}) {
+	constructor(
+		scene, {sigma, beta, rho},
+		{time_step = 0.01, points_amount = 10, region_radius = 60} = {}
+	) {
 		this.scene = scene;
-		this.sigma = sigma;
-		this.beta = beta;
-		this.rho = rho;
-
-		// TODO: Add multiple points
-		this.x = 0.01;
-		this.y = 0.01;
-		this.z = 0.01;
 
 		// TODO: Remove in favor of lines
 		this.geometry = new THREE.SphereGeometry(0.25, 10, 5);
 		this.material = new THREE.MeshBasicMaterial({color: 0x5555ff});
 
 		this.enclosing = new THREE.Mesh(
-			new THREE.SphereGeometry(60, 64, 32),
+			new THREE.SphereGeometry(region_radius, 64, 32),
 			new THREE.MeshBasicMaterial({color: 0xb04343, wireframe: true})
 		);
 		this.enclosing.position.z = 30;
 		scene.add(this.enclosing);
+
+		this.sigma = sigma;
+		this.beta = beta;
+		this.rho = rho;
+		this.delta_t = time_step;
+
+		this.points = [];
+		for (let i = 0; i < points_amount; i++) {
+			let color = Math.round(Math.random() * 0xffffff);
+
+			// Generate a point in the 2D xz-plane
+			let xz_angle = Math.random() * 2*Math.PI;
+			let xz_radius = Math.random() * region_radius;
+			let [x, z] = [
+				Math.cos(xz_angle) * xz_radius,
+				Math.sin(xz_angle) * xz_radius + 30	// TODO: Specify where 30 comes from
+			];
+
+			// Rotate the point in the 2D xy-plane
+			let yx_angle = Math.random() * 2*Math.PI;
+			let yx_radius = x;
+			let y = Math.sin(yx_angle) * yx_radius;
+			x = Math.cos(yx_angle) * yx_radius;
+
+			this.points.push([new THREE.Vector3(x, y, z), color]);
+		}
 	}
 
-	step(delta_t) {
-		let delta_x = this.sigma * (this.y - this.x);
-		let delta_y = this.x * (this.rho - this.z) - this.y;
-		let delta_z = this.x * this.y - this.beta * this.z;
+	step() {
+		for (let i = 0; i < this.points.length; i++) {
+			let point = this.points[i][0];
 
-		this.x += delta_x * delta_t;
-		this.y += delta_y * delta_t;
-		this.z += delta_z * delta_t;
+			let delta = new THREE.Vector3(
+				this.sigma * (point.y - point.x),
+				point.x * (this.rho - point.z) - point.y,
+				point.x * point.y - this.beta * point.z
+			);
+			delta.multiplyScalar(this.delta_t);	// Multiply every component by delta_t
+
+			point.add(delta);	// Offset the components by their respective deltas
+		}
 	}
 
 	update() {
-		let point = new THREE.Mesh(this.geometry, this.material);
-		point.position.x = this.x;
-		point.position.y = this.y;
-		point.position.z = this.z;
-		scene.add(point);
+		for (let i = 0; i < this.points.length; i++) {
+			let [point, color] = this.points[i];
+			let object = new THREE.Mesh(
+				this.geometry,
+				new THREE.MeshBasicMaterial({color: color})
+			);
+			object.position.copy(point);
+			scene.add(object);
+		}
 
-		this.step(0.01);
+		this.step();
 	}
 }
